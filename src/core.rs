216d53dd3687d4964c76aeccc95ef7a513d4b1ce
kusogakiller <negiradomoti@proton.me>
2026-07-06 05:49:51 +0800

    Update core.rs

--- src/core.rs
+++ src/core.rs
@@ -1,12 +1,17 @@
 use rand::RngCore;
 use blake3;
+use hex;
 
-pub fn generate(len: usize) -> String {
-    let mut raw = vec![0u8; 64];
+pub fn generate(size: usize) -> String {
+    let mut raw = vec![0u8; size];
     rand::thread_rng().fill_bytes(&mut raw);
 
     let hash = blake3::hash(&raw);
     let hex = hash.to_hex().to_string();
 
-    hex[..len].to_string()
+    match size {
+        32 => hex[..32].to_string(),
+        64 => hex[..64].to_string(),
+        _ => hex[..32].to_string(),
+    }
 }
